c20925455874a2b6613776e935b66e9d4adb26d5
Valentine Wu <valentine.business@gmail.com>
2020-06-02 10:22:51 -0700

    Update startup-json.sh

--- custom-resource/get-bootstrap-brokers/startup-json.sh
+++ custom-resource/get-bootstrap-brokers/startup-json.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation create-stack --stack-name MSK-Bootstrap-Stack --template-body file://3az-default-with-client-bootstrap-brokers.json --parameters ParameterKey="MSKClusterName",ParameterValue="MSK-Bootstrap" --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name MSK-Bootstrap-Stack --template-body file://3az-default-with-client.json --parameters ParameterKey="MSKClusterName",ParameterValue="MSK-Bootstrap" --capabilities CAPABILITY_IAM
