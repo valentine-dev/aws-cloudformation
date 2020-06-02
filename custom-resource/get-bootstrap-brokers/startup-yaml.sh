5d3434b6f7a75b8ece7df69ec0ca90670eb27d81
Valentine Wu <valentine.business@gmail.com>
2020-06-02 10:23:13 -0700

    Update startup-yaml.sh

--- custom-resource/get-bootstrap-brokers/startup-yaml.sh
+++ custom-resource/get-bootstrap-brokers/startup-yaml.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation create-stack --stack-name MSK-Bootstrap-Stack --template-body file://3az-default-with-client-bootstrap-brokers.yaml --parameters ParameterKey="MSKClusterName",ParameterValue="MSK-Bootstrap" --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name MSK-Bootstrap-Stack --template-body file://3az-default-with-client.yaml --parameters ParameterKey="MSKClusterName",ParameterValue="MSK-Bootstrap" --capabilities CAPABILITY_IAM
